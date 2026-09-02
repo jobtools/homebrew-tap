f244494de82575ea9009504393756a8ee8b68a89
ssk <developer.kss@gmail.com>
2026-09-02 11:42:02 +0900

    airplay-touch 1.5.24

--- Casks/airplay-touch.rb
+++ Casks/airplay-touch.rb
@@ -1,8 +1,8 @@
 cask "airplay-touch" do
-  version "1.5.23"
-  sha256 "32d1e4d12e4dcd8d40bd894b8386e6c766dd8882ed22ff03fa0d63370dec7880"
+  version "1.5.24"
+  sha256 "20f7d117384287b0208d041b7bf58fcc85f6ec8acb708fd3b2a0465d5030a775"
 
-  url "https://github.com/jobtools/homebrew-tap/releases/download/airplay-touch-v#{version}/AirPlay-Touch-#{version}.zip"
+  url "https://github.com/jobtools/homebrew-tap/releases/download/airplay-touch-v#{version}/AirPlay-Touch-#{version}.dmg"
   name "AirPlay Touch"
   desc "AirPlay receiver companion that pairs with the Android app"
   homepage "https://apps.sskplay.com/airplay-touch/"
